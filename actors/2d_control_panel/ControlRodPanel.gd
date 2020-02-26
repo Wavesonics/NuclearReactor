eea715982bec83b0da7351133dda2282af5cb808
Adam Brown <adamwbrown@gmail.com>
2020-02-25 21:59:59 -0700

    Indicator light code is more robust now

--- actors/2d_control_panel/ControlRodPanel.gd
+++ actors/2d_control_panel/ControlRodPanel.gd
@@ -1,33 +1,23 @@
 extends HBoxContainer
 class_name ControlRodPanel
 
+var lightScene := preload("res://actors/2d_control_panel/IndicatorLight.tscn")
+
 var controlRod: ControlRod
-var indicatorLights := []
+var numLights := 10
 
 
 func _ready():
-	indicatorLights.append($LightsContainer/IndicatorLight0)
-	indicatorLights.append($LightsContainer/IndicatorLight1)
-	indicatorLights.append($LightsContainer/IndicatorLight2)
-	indicatorLights.append($LightsContainer/IndicatorLight3)
+	for ii in range(0, numLights):
+		var node := lightScene.instance()
+		$LightsContainer.add_child(node)
 
 
 func _process(delta):
 	var p := controlRod.percent_out() as float
-	
-	if p == 1.0:
-		turn_lights_on(0)
-	elif p > 0.75:
-		#print("75")
-		turn_lights_on(1)
-	elif p > 0.50:
-		#print("50")
-		turn_lights_on(2)
-	elif p > 0.0:
-		#print("25")
-		turn_lights_on(3)
-	else:
-		turn_lights_on(4)
+	var s := 1.0 / numLights
+	var x = (p/s) as int
+	turn_lights_on((numLights-x)+1)
 
 
 func _physics_process(delta):
@@ -38,5 +28,5 @@ func _physics_process(delta):
 
 
 func turn_lights_on(n: int):
-	for ii in range(0, indicatorLights.size()):
-		indicatorLights[ii].set_on(n > 0 && (ii+1 <= n || ii+1 == n))
+	for ii in range(0, numLights):
+		$LightsContainer.get_child(ii).set_on(n > 0 && ii+1 < n)
