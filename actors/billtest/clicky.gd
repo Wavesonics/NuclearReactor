47e34277b07f1ac05ebb86258a0392528a4b9c6c
Bill <moronbros@gmail.com>
2020-03-16 22:35:23 -0700

    proper operator

--- actors/billtest/clicky.gd
+++ actors/billtest/clicky.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var audio = get_node("GeigerCounter")
+	var audio := get_node("GeigerCounter") as AudioStreamPlayer
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
