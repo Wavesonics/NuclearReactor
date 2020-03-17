e4ae9964c7edc51b372dd917882417f6570c574e
Adam Brown <adamwbrown@gmail.com>
2020-03-16 23:05:56 -0700

    Fill buffer example

--- actors/billtest/clicky.gd
+++ actors/billtest/clicky.gd
@@ -1,16 +1,29 @@
-extends Node2D
+extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+# Declare member variables here.
+onready var audio := $GeigerCounter as AudioStreamPlayer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var audio := get_node("GeigerCounter") as AudioStreamPlayer
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Button_pressed():
+	test_audio()
+
+
+func test_audio():
+	var playback := audio.get_stream_playback()
+	
+	fill_buffer(playback)
+	
+	audio.play()
+
+
+func fill_buffer(playback):
+	var to_fill = playback.get_frames_available()
+	while (to_fill > 0):
+		playback.push_frame( Vector2(1.0,1.0)) 
+		to_fill-=1;
